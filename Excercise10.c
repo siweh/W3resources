ce9a89a9c954e38c5da8fd07a3d117d3294abe27
siweh <sanemadesi@gmail.com>
2019-02-19 12:18:21 +0200

    Excercise 10

--- Excercise10.c
+++ Excercise10.c
@@ -7,3 +7,20 @@ int multiplication (int num1,int num2)
 }
 
 int main()
+{
+	int num1,num2,product;
+
+	printf("Please enter two numbers: ");
+
+	printf("\nNumber 1: ");
+	scanf("%d",&num1);
+
+	printf("\nNumber 2: ");
+	scanf("%d",&num2);
+
+	product = multiplication(num1,num2);
+
+	printf("The product of the two numbers is %d",product);
+
+	return 0;
+}
